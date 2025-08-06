eeda1a242a82f7cfac031752d2b989c468a7a80f
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-08-06 20:16:57 +0500

    loading added

--- src/components/StudentView.tsx
+++ src/components/StudentView.tsx
@@ -5,6 +5,8 @@ import { type IData } from '../types';
 import { MdTableRows, MdDelete } from "react-icons/md";
 import { PiCardsBold } from "react-icons/pi";
 import { RiEdit2Fill } from "react-icons/ri";
+import CardSkeleton from './ui/CardSkeleton';
+import TableSkeleton from './ui/TableSkeleton';
 
 interface IProps {
     students: IData[]
@@ -39,51 +41,61 @@ const StudentView: FC<IProps> = ({ students, handleDelete, setUpdateStudent }) =
                                     <th>Delete</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                {
-                                    students?.map((student, index) => (
-                                        <tr key={student.id} className="border-b hover:bg-slate-600">
-                                            <td className="py-3 px-6">{index + 1}</td>
-                                            <td className="py-3 px-6">{student.fname}</td>
-                                            <td className="py-3 px-6">{student.profession}</td>
-                                            <td className="py-3 px-6">{student.gender}</td>
-                                            <td onClick={() => setUpdateStudent(student)} className="text-center"><button><RiEdit2Fill /></button></td>
-                                            <td onClick={() => handleDelete(student.id)} className="text-center"><button><MdDelete /></button></td>
-                                        </tr>
-                                    ))
-                                }
-                            </tbody>
+                            {
+                                students.length == 0 ? <TableSkeleton /> : (
+                                    <tbody>
+                                        {
+                                            students?.map((student, index) => (
+                                                <tr key={student.id} className="border-b hover:bg-slate-600">
+                                                    <td className="py-3 px-6">{index + 1}</td>
+                                                    <td className="py-3 px-6">{student.fname}</td>
+                                                    <td className="py-3 px-6">{student.profession}</td>
+                                                    <td className="py-3 px-6">{student.gender}</td>
+                                                    <td onClick={() => setUpdateStudent(student)} className="text-center"><button><RiEdit2Fill /></button></td>
+                                                    <td onClick={() => handleDelete(student.id)} className="text-center"><button><MdDelete /></button></td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                )
+                            }
                         </table>
                     </div>
                 ) : (
-                    <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-3">
+                    <>
                         {
-                            students?.map((student) => (
-                                <div key={student.id} className="p-4 bg-slate-800 rounded-xl ">
-                                    <div className="relative">
-                                        <img
-                                            className="size-40 object-cover mx-auto rounded-full"
-                                            src={student.gender == "male" ? maleImage : femaleImage}
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div className="text-center mt-4">
-                                        <h3 className="font-bold text-xl">{student.fname}</h3>
-                                        <p className="my-2 text-gray-500">{student.profession}</p>
+                            students.length == 0 ? <CardSkeleton /> : (
+                                <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-3">
+                                    {
+                                        students?.map((student) => (
+                                            <div key={student.id} className="p-4 bg-slate-800 rounded-xl">
+                                                <div className="relative">
+                                                    <img
+                                                        className="size-40 object-cover mx-auto rounded-full"
+                                                        src={student.gender == "male" ? maleImage : femaleImage}
+                                                        alt=""
+                                                    />
+                                                </div>
+                                                <div className="text-center mt-4">
+                                                    <h3 className="font-bold text-xl">{student.fname}</h3>
+                                                    <p className="my-2 text-gray-500">{student.profession}</p>
 
-                                        <div className="flex gap-2 mt-4">
-                                            <button onClick={() => handleDelete(student.id)} className="py-0.5 border rounded-lg text-sm flex-1 text-red-500">
-                                                Delete
-                                            </button>
-                                            <button onClick={() => setUpdateStudent(student)} className="py-0.5 border rounded-lg text-sm flex-1 text-green-700">
-                                                Update
-                                            </button>
-                                        </div>
-                                    </div>
+                                                    <div className="flex gap-2 mt-4">
+                                                        <button onClick={() => handleDelete(student.id)} className="py-0.5 border rounded-lg text-sm flex-1 text-red-500">
+                                                            Delete
+                                                        </button>
+                                                        <button onClick={() => setUpdateStudent(student)} className="py-0.5 border rounded-lg text-sm flex-1 text-green-700">
+                                                            Update
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
                                 </div>
-                            ))
+                            )
                         }
-                    </div>
+                    </>
                 )
             }
         </>
